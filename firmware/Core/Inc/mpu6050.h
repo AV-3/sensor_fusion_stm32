0188126e480b08f9161fbee315c4b8369474212c
Alan <avelan.lne@gmail.com>
2025-07-26 23:38:24 -0500

    unignored firmware folder

---
+++ firmware/Core/Inc/mpu6050.h
@@ -0,0 +1,13 @@
+/*
+ * mpu6050.h
+ *
+ *  Created on: Jul 26, 2025
+ *      Author: avelan
+ */
+
+#ifndef INC_MPU6050_H_
+#define INC_MPU6050_H_
+
+
+
+#endif /* INC_MPU6050_H_ */
---
+++ firmware/Core/Src/mpu6050.c
@@ -0,0 +1,8 @@
+/*
+ * mpu6050.c
+ *
+ *  Created on: Jul 26, 2025
+ *      Author: avelan
+ */
+
+
